085b4a6b17cfde6275d2597eae49b326dc76a560
Shely Dexter <micheleannedexter@gmail.com>
2019-05-07 17:39:34 -0400

    even-number

--- problems/06-even-number/06-even-number.js
+++ problems/06-even-number/06-even-number.js
@@ -2,6 +2,16 @@
 Define function: isEven(someNum)
 Return true if even, false if odd.
 Do not use % operator. */
+
 function isEven(someNum) {
-  
+    let num = someNum + '';
+    if (num.match(/[\.]+/) !== null) return false;
+    return num.match(/[02468]$/) !== null;
 }
+
+console.log(isEven(2468));
+console.log(isEven(1357));
+console.log(isEven(0));
+console.log(isEven(-5001));
+console.log(isEven(-8));
+console.log(isEven(5.8));
