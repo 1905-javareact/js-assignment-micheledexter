f88bc981a959280496715e1905e178aca5b15ba8
Shely Dexter <micheleannedexter@gmail.com>
2019-05-07 16:05:19 -0400

    fibonacci

--- problems/01-fibonacci/01-fibonacci.js
+++ problems/01-fibonacci/01-fibonacci.js
@@ -1,6 +1,16 @@
 /* 1. Fibonacci
 Define function: fib(n) 
 Return the nth number in the fibonacci sequence. */
-function fib(n) {
 
+function fib(n) {
+    // if (n <= 0) return 0; // if you want the sequence to start at 0, uncomment this line
+    if (n <= 1) return 1;
+    return fib(n-1) + fib(n-2);
+}
+
+/*
+Test out the code
+*/
+for (let i = 0; i < 20; i++) {
+    console.log(`fib(${i}) = ${fib(i)}`);
 }
