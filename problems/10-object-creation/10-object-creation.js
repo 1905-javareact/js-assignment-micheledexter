4a9d6ea4e47bb9ac0c3abda7ff5ffc13ac79a03a
Shely Dexter <micheleannedexter@gmail.com>
2019-05-07 21:09:30 -0400

    object-creation

--- problems/10-object-creation/10-object-creation.js
+++ problems/10-object-creation/10-object-creation.js
@@ -1,3 +1,39 @@
 /* 10 Object Creation
 Create an object using an object literal, constructor function, and es6 class.
 The object should have the fields name and age.*/
+
+let homer = {
+    name: 'Homer',
+    occupation: 'Nuclear Safety Inspector',
+    kids: 3,
+    hobby: 'Drinking Beer'
+}
+
+function createPerson(name, occupation, kids, hobby) {
+    this.name = name;
+    this.occupation = occupation,
+    this.kids = kids;
+    this.hobby = hobby;
+}
+
+let stewie = new createPerson('Stewie', 'Being a baby', 0, 'Tormenting Lois');
+
+class Person {
+    constructor(name, occupation, kids, hobby) {
+        this.name = name;
+        this.occupation = occupation;
+        this.kids = kids;
+        this.hobby = hobby;
+    }
+
+    toString() {
+        return `Name: ${this.name}\nOccupation: ${this.occupation}\nKids: ${this.kids}\nHobby: ${this.hobby}`;
+    }
+}
+
+let ashley = new Person('Laura', 'Voice Actress', 1, 'Dungeons & Dragons');
+
+console.log(homer);
+console.log(stewie);
+console.log(ashley);
+console.log(ashley.toString());
