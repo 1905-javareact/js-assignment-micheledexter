64566ee55c01f74b7904d8953fd5e08c7004981b
Shely Dexter <micheleannedexter@gmail.com>
2019-05-08 16:45:31 -0400

    factorial

--- problems/04-factorial/04-factorial.js
+++ problems/04-factorial/04-factorial.js
@@ -3,10 +3,16 @@ Define function: factorial(someNum)
 Use recursion to compute and return the factorial of someNum.  */
 
 function factorial(sumNum) {
+    if (isNaN(sumNum) || Math.abs(sumNum) === Infinity) throw `Please use a positive number, ${sumNum} is not valid.`;
     if (sumNum <= 1) return sumNum;
     return sumNum * factorial(sumNum - 1);
 }
 
 for (let i = 1; i <= 10; i++) {
     console.log(`factorial(${i}) = ${factorial(i)}`);
+}
+try {
+    console.log(`factorial('cheese') = ${factorial('cheese')}`);
+} catch(e) {
+    console.log(e);
 }
