d3c87ea5c2434f4a96819c311c96964c52e38197
Shely Dexter <micheleannedexter@gmail.com>
2019-05-07 21:35:00 -0400

    splice-element

--- problems/11-splice-element/11-splice-element.js
+++ problems/11-splice-element/11-splice-element.js
@@ -2,6 +2,26 @@
 Define function spliceElement(someArr, index)
 Splice the element at the provided index in the array.
 */
+
+// for this, I'm assuminng we're writing our own splice method and not just
+// using the splice prototype method.
+
 function spliceElement(someArr, index) {
-  
+    let newArr = [];
+    let spliced = null;
+    for (let i = someArr.length - 1; i >= 0; i--) {
+        if (i === index) {
+            spliced = someArr.pop();
+        } else {
+            newArr.unshift(someArr.pop());
+        }
+    }
+    for (let i = newArr.length - 1; i >= 0; i--) {
+        someArr.unshift(newArr.pop());
+    }
+    return spliced;
 }
+
+let alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g'];
+console.log(spliceElement(alpha, 4));
+console.log(alpha);
