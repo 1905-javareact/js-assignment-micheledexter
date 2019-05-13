7495d49f775aa958901a9040c4b1bdc4f3deca37
Michele Dexter <micheleannedexter@gmail.com>
2019-05-13 12:39:36 -0400

    object-properties

--- problems/09-object-properties/09-object-properties.js
+++ problems/09-object-properties/09-object-properties.js
@@ -3,7 +3,7 @@ Define function objectProperties(someObj)
 Print every property and it's value. */
 
 function objectProperties(someObj) {
-    if (typeof(someObj) !== 'object') throw 'Please use an object as input, if it\'s in JSON format, please parse it first';
+    if (typeof(someObj) !== 'object' || Array.isArray(someObj)) throw 'Please use an object as input, if it\'s in JSON format, please parse it first';
     for (let item in someObj) {
         console.log(`someObj[${item}] has a value of ${someObj[item]}`);
     }
